9cb9df343b1091f5611f78dd2c4c6ec17f6cf161
rlharsh <ronald.harsh@protonmail.com>
2023-10-26 09:18:38 -0500

    Update scrolling behavior

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const API_KEY = '3264a131020d738a0896d5230d569d50'; // Replace with your TMDb API key
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie`;
 const POPULAR_MOVIES_URL = `https://api.themoviedb.org/3/movie/popular`;
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
-const NOW_PLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+const NOW_PLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US';
 const TRENDING_URL = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+const TOP_RATED_URL = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US';
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
 
 const BACKDROP_SIZE = 'original';
 const POSTER_SIZE = 'w342';
 
+const nowPlayingSection = document.getElementById('app-container');
+
+document.addEventListener('DOMContentLoaded', async function(e) {
+    document.addEventListener('scroll', function(e) {
+        let documentHeight = document.body.scrollHeight;
+        let currentScroll = window.scrollY + window.innerHeight;
+        let modifier = 200;
+        if(currentScroll + modifier > documentHeight) {
+            if (currentPage < totalPages) {
+                currentPage++;
+                 setContainerMovies('now_playing');
+              }
+        }
+    })
+})
+
+
+nowPlayingSection.addEventListener('scroll', async () => {
+  if (nowPlayingSection.offsetHeight + nowPlayingSection.scrollTop <= nowPlayingSection.scrollHeight) {
+    console.log("HI");
+    if (currentPage < totalPages) {
+      currentPage++;
+      await setContainerMovies('now_playing');
+    }
+  }
+});
 /* Buttons */
 const SEARCH_BUTTON = document.getElementById('search-button');
 SEARCH_BUTTON.addEventListener('click', searchClick);
@@ -27,10 +55,40 @@ const options = {
 
 
 async function entryPoint() {
-    await getTrendingMovies();
+    //await getTrendingMovies();
     await getNowPlaying();
+    await setContainerMovies('top_rated');
 }
 
+async function setContainerMovies(list = 'now_playing') {
+    if (list === "now_playing") {
+        const movieData = await getMovieData(NOW_PLAYING_URL);
+        processMovieResults(movieData);
+    } else if (list === "upcoming") {
+
+    } else if (list === "top_rated") {
+        const movieData = await getMovieData(TOP_RATED_URL);
+        processMovieResults(movieData);
+    } else if (list === "popular") {
+
+    }
+}
+
+async function getMovieData(URL) {
+    try {
+        const response = await fetch(`${URL}&page=${currentPage}`, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data && data.results) {
+            totalPages = data.total_pages;
+            return data;
+        }
+    } catch (error) {
+        console.log('Error fetching data: ', error);
+    }
+}
 
 async function searchClick(e) {
     const SEARCH_INPUT = document.getElementById('app-search-input');
@@ -56,25 +114,6 @@ const getNowPlaying = async () => {
         const data = await response.json();
         if (data && data.results) {
             processNowPlaying(data);
-        }
-    } catch (error) {
-        console.error('Error fetching search data: ', error);
-    }
-};
-
-const getTrendingMovies = async () => {
-    try {
-        const response = await fetch(TRENDING_URL, options);
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        if (data && data.results) {
-            currentPage = data.page;
-            totalPages = data.total_pages;
-            processMovieResults(data);
         }
     } catch (error) {
         console.error('Error fetching search data: ', error);
